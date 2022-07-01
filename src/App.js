46a220b7b1a5ee44ef9580b50436327c31194279
Radoslaw Lenik <rleni@2FJJBK3.localdomain>
2022-07-01 14:06:18 +0200

    changing text in js file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Yolo
+         Changed data to see if AWS wgets updated.
         </p>
         <a
           className="App-link"
